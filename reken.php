5ae571d2d50d46c97747452a61fe982c7f562abc
Josiah6317 <josiah.richardson@student.gildeopleidingen.nl>
2026-05-29 09:36:48 +0200

    Update reken.php

--- reken.php
+++ reken.php
@@ -2,16 +2,15 @@
 // Simple Calculator
 echo "Simple Calculator\n";
 
-// Get user inputs
 function num1() {
-$num1 = (float)readline("Enter first number: ");
+return (float)readline("Enter first number: ");
 }
 
 function operation(){
-$operation = readline("Enter operation (+, -, *, /): ");
+return readline("Enter operation (+, -, *, /): ");
 }
 function num2(){
-$num2 = (float)readline("Enter second number: ");
+return (float)readline("Enter second number: ");
 }
 
 
@@ -20,12 +19,28 @@ function calculate() {
     $operation = operation();
     $num2 = num2();
 
-    if ($operation == "+") $result = $num1 + $num2;
+    if ($operation == "+")  $result = $num1 + $num2;
     elseif ($operation == "-") $result = $num1 - $num2;
     elseif ($operation == "*") $result = $num1 * $num2;
     elseif ($operation == "/") $result = $num1 / $num2;
     else $result = "Invalid operator";
+    return $result;
 
-    echo "Result: " . $result;
 }
+$again = "y";
+ while ($again === "y"){
+
+    $result = calculate();
+
+    echo "Result: $result\n";
+   $again = strtolower(readline("Calculate again? (y/n): ")); //added $again so that you can actually close the terminal
+    
+
+ }
+ 
+if ($again === "n"){
+        echo "goodbye!\n";
+        exit();
+    }
+
  ?>
